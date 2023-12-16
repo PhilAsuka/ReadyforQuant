88269bc520762faccf6a778eaf8789a3830134a4
yooooohning <paranoidzh@gmail.com>
2023-12-17 01:35:52 +0800

    Update on 12.11

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 # app/core/config.py
 
-DATABASE_URL = "mysql+aiomysql://root:Zhangzehang1234...@gz-cynosdbmysql-grp-2un1406t.sql.tencentcdb.com:21864/market_data_ETH_USDT_SWAP"
-API_KEY = "213e929c-e2b3-4b35-8b7e-ff41cce21751"
-SECRET_KEY = "A0560094E87C19D4DB8D74C346990355"
-PASSPHRASE = "Zhangzehang1234..."
+DATABASE_URL = ""
+API_KEY = ""
+SECRET_KEY = ""
+PASSPHRASE = ""
